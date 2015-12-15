446e1b15a1a08083bc8d9703c3fbe22f5bc8d9fb
alexlashford <alexl@cakesolutions.net>
2015-12-15 17:27:15 +0000

    example code to show the problem

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
 akka-cluster-demo
 =================
 
+
 Testing the Akka 2.4.0 feature ["akka.cluster.sharding.remember-entities"](http://doc.akka.io/docs/akka/current/scala/cluster-sharding.html#Remembering_Entities).
+
+
+###Requirements:
+
+SBT : sbt.version=0.13.8
+Cassandra : The apps expects a cassandra instance running on ```localhost```.
+
+
+###Re-creating the error
+
+There are two main classes that can run that create a two node cluster.
+
+```Dc1Cluster```
+
+```Dc2Cluster```
+
+note:   You need to set the following VM args before launching the apps
+
+```
+Dc1Cluster >>>
+-Dcom.sun.management.jmxremote.port=9998 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false
+```
+
+```
+Dc2Cluster >>>
+-Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false
+```
+
+Once the above is configured, run both apps.
+
+Fire up ```jconsole``` connect to the leader (usually Dc1)  so ```localhost:9998```
+
+From here select `mbeans` > `akka` > `cluster` > `operations`
+
+then call `Leave("akka.tcp://KlasterSystem@127.0.0.1:2551")`
+
+This will cause the leader to handover the Shared Regions that it has and pass them to the another Node, this other node also becomes the new leader.
+
+Handover is successful but we also want to shutdown the jvm once handover and cluster removal is finished!
+
+Problems Observed:
+
+> Problem seems to be the `registerOnMemberRemoved` event is called straightaway even though Shard Region handoff has not completed
+> Cluster status of the node that was removed never gets passed `Exiting`
+> New leader node never sends `remove` message to other Node as per diagram http://doc.akka.io/docs/akka/snapshot/common/cluster.html#State_Diagram_for_the_Member_States__akka_cluster_allow-weakly-up-members=off_
+> New leader marks old node as UNREACHABLE after the heartbeat timeout which then removes it from the cluster.
+
+> have tried using graceful shutdown with even less success, this seems to cause the actor system to close before handoff complete.
--- src/main/scala/com/gvolpe/cluster/management/ClusterManagement.scala
+++ src/main/scala/com/gvolpe/cluster/management/ClusterManagement.scala
@@ -1,7 +1,7 @@
 package com.gvolpe.cluster.management
 
 import akka.actor.ActorSystem
-import akka.cluster.Cluster
+import akka.cluster.{MemberStatus, Cluster}
 import com.gvolpe.cluster.actors.GracefulShutdownActor
 
 import scala.concurrent.Await
@@ -13,25 +13,42 @@ class ClusterManagement(system: ActorSystem, port: Int) extends ClusterManagemen
   override def leaveClusterAndShutdown(): Unit = {
     println(s"INVOKING MBEAN ${system.name}")
 
+    // This should perform a graceful shutdown, and once Shard regions are handed over to the other Node, this node should leave the cluster.
     val shutdownActor = system.actorOf(GracefulShutdownActor.props)
     shutdownActor ! GracefulShutdownActor.LeaveAndShutdownNode
+
+    // This is the equivilent of just using the existing Akka Mbean, but you don't have to pass the URI
 //    cluster.leave(cluster.selfAddress)
   }
 
 
-//  private val cluster:Cluster = {
-//    val cluster:Cluster = Cluster(system)
-//    cluster.registerOnMemberRemoved {
-//      system.registerOnTermination(System.exit(0))
-//      system.terminate()
-//      new Thread {
-//        override def run(): Unit = {
-//          if (Try(Await.ready(system.whenTerminated, 10.seconds)).isFailure)
-//            System.exit(-1)
+  private val cluster:Cluster = {
+    val cluster:Cluster = Cluster(system)
+    cluster.registerOnMemberRemoved {
+
+
+      println(s">>>>>>> ${cluster.state.members.find(_.address == cluster.selfAddress)}")
+
+      // bit of hack to wait for status to change.
+//      while(true){
+//        println(s">>>>>>> ${cluster.state.members.find(_.address == cluster.selfAddress)}")
+//        if(cluster.state.members.find(_.address == cluster.selfAddress).get.status == MemberStatus.removed){
+//
+//          // Shutdown the JVM with failover ((@see http://doc.akka.io/docs/akka/snapshot/scala/cluster-usage.html#How_To_Cleanup_when_Member_is_Removed))
+//          system.registerOnTermination(System.exit(0))
+//          system.terminate()
+//          new Thread {
+//            override def run(): Unit = {
+//              if (Try(Await.ready(system.whenTerminated, 10.seconds)).isFailure)
+//                System.exit(-1)
+//            }
+//          }.start()
+//
 //        }
-//      }.start()
-//    }
-//    cluster
-//  }
+//        Thread.sleep(3000)
+//      }
+    }
+    cluster
+  }
 
 }
